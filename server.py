b48d552fd36a829f25942655f6bc6dba2d278075
Roman Barinov <rbarinov2004@yahoo.com>
2023-10-29 18:30:41 -0400

    Finished lab

--- server.py
+++ server.py
@@ -2,27 +2,37 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-#Initiate the flask app : TODO
+#Initiate the flask app
+app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
+    ''' This is a handler of incoming request "/sentimentAnalyzer"
     '''
-    # TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+    if (text_to_analyze is None) or (len(text_to_analyze.strip()) == 0):
+        return "No input supplied!"
+
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    if label is None:
+        return "Invalid input ! Try again."
+
+    #return "The given text has been identified as {} with a score of {}." \
+    #    .format(label.split('_')[1], score)
+    return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    # This functions executes the flask app and deploys it on localhost:5000
+    app.run(host="0.0.0.0", port=5000)
